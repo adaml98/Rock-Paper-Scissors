0f5ec9ed7648b6fba802589e66925ad0d9ee9f0a
Adam Lindley <adamlindley98@gmail.com>
2022-05-03 18:54:25 +0100

    On screen round results - removed console logs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function randomNo() {
     return Math.floor(Math.random() * 3) + 1;
 }
 
-
+//function for computer selection
 function computerPlay() {
     if (randomNo() === 1) {
         return "rock";
@@ -19,7 +19,6 @@ function computerPlay() {
 
 //function for each round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
-    //console.log(button.id);
     //compare selections
     if (playerSelection == computerSelection) {
         return result = `You draw! You both picked ${playerSelection}`;//output outcome of game
@@ -38,55 +37,49 @@ function playRound(playerSelection, computerSelection) {
 function game() {
     let playerScore = 0;
     let computerScore = 0;
-    let i = 0;
-    //for(i = 0; i < 5; i++){ //Play 5 rounds
-
-
+    let i =1;
+    const results = document.querySelector("#results"); // reference results div that already exists
     //button listeners
     const buttons = document.querySelectorAll("button");
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
             playerSelection = button.id;
-            console.log(playerSelection);
             computerSelection = computerPlay();
             playRound();
-            alert(playRound(playerSelection, computerSelection));
-            //console.log(`Player Score: ${playerScore}`);
-            //console.log(`Computer Score: ${computerScore}`);
-            console.log(playRound(playerSelection, computerSelection));
             if (result === `You win! ${playerSelection} beats ${computerSelection}`) {
-                playerScore++;
+                playerScore+=2;
 
             } else if (result === `You lose! ${computerSelection} beats ${playerSelection}`) {
+                computerScore+=2;
+            } else {
+                playerScore++;
                 computerScore++;
             }
-            alert(`Player Score: ${playerScore} Computer Score: ${computerScore}`);
-            i++
-            console.log(i);
+        
+            const presult = document.createElement("p");
+            
+            results.appendChild(presult);
+            presult.textContent = `Round ${i}: ${playRound(playerSelection, computerSelection)}`
+             //`Player Score: ${playerScore} Computer Score: ${computerScore}`;
+            i++;
         });
     });
 
     //}// 5 rounds exit bracket
     /*
-                if(playerScore > computerScore){
+                if(playerScore === 5){
                     alert("You win the game!");
                     console.log("..........");
                     console.log("Game Over!");
                     console.log("..........");
                     console.log("You win!");
-                } else if (playerScore < computerScore){
+                } else if (computerScore === 5){
                     alert("You lose the game! ");
                     console.log("..........");
                     console.log("Game Over!");
                     console.log("..........");
                     console.log("You lose!");
-                } else if (playerScore === computerScore){
-                    alert("The game is a draw!");
-                    console.log("..........");
-                    console.log("Game Over!")
-                    console.log("..........");
-                    console.log("Draw!");
-                }
+               
     */
 }
 
